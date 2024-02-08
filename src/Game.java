00c9366b8d8c6907dd8e350b6656754cb98b7ba7
Daniel Pawłowicz <danielpawlowicz@interia.pl>
2024-02-08 10:50:13 +0100

    Initial methods

--- src/Game.java
+++ src/Game.java
@@ -3,11 +3,29 @@ import java.awt.event.ActionListener;
 
 public class Game implements ActionListener{
 
-	
+	Game(){
+		
+	}
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
+		
+	}
+	
+	public void firstTurn() {
+		
+	}
+	
+	public void check() {
+		
+	}
+	
+	public void xWins(int a, int b, int c) {
+		
+	}
+	
+	public void yWins(int a, int b, int c) {
 		
 	}
 
